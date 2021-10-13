1e2a73f8f8274e4c3958e4dab13096254002fe94
markiplagat <markkiplagatkipruttoh@gmail.com>
2021-10-13 17:32:27 +0300

    Get neighbours of a cell

--- js/gameoflife.js
+++ js/gameoflife.js
@@ -45,7 +45,10 @@ const printCells = (state) => {
   return ac
 };
 
-const getNeighborsOf = ([x, y]) => {};
+const getNeighborsOf = ([x, y]) => [
+  [x-1, y-1], [x, y+1], [x+1, y+1], [x-1, y],
+  [x+1, y], [x-1, y-1], [x+1, y-1]
+];
 
 const getLivingNeighbors = (cell, state) => {};
 
